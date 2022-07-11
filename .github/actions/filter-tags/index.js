135f2250cc8208413deecbbab1a65ae651ff83b8
Marcel Kinzel <kinzel@easy-cube.de>
2022-07-11 22:47:28 +0200

    added custom action matrix output

--- .github/actions/filter-tags/index.js
+++ .github/actions/filter-tags/index.js
@@ -9,14 +9,17 @@ try {
   );
 
   let filteredTags = [];
+  let matrix = { include: [] };
   tagsString.split(",").forEach((tag) => {
     if (tag.match(`^${major}(?:$|\\.)`)) {
-      filteredTags.push({
+      filteredTags.push(tag);
+      matrix.include.push({
         tag: tag,
       });
     }
   });
   core.setOutput("tags", JSON.stringify(filteredTags));
+  core.setOutput("matrix", JSON.stringify(matrix));
 } catch (error) {
   core.setFailed(error.message);
 }
